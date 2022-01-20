ed370b69aa1ab5dd3299dc6d20f0a66795619de2
Moks <gutamax2@mail.ru>
2022-01-20 09:08:07 +0500

    initial commit

---
+++ src/test/java/ru/nethology/web/ChromeTest.java
@@ -0,0 +1,34 @@
+package ru.nethology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverInfo;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class ChromeTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll(){
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.quit();
+        driver = null;
+    }
+}
