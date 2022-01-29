40f22ba3428995aa5e3ca9d8a02526c7b5a786fa
Moks <gutamax2@mail.ru>
2022-01-29 10:51:25 +0500

    add selectors

--- src/test/java/ru/nethology/web/ChromeTest.java
+++ src/test/java/ru/nethology/web/ChromeTest.java
@@ -4,10 +4,16 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverInfo;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ChromeTest {
     private WebDriver driver;
@@ -20,9 +26,9 @@ public class ChromeTest {
     @BeforeEach
     public void setUp(){
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+//        options.addArguments("--disable-dev-shm-usage");
+//        options.addArguments("--no-sandbox");
+//        options.addArguments("--headless");
         driver = new ChromeDriver(options);
     }
 
@@ -30,5 +36,19 @@ public class ChromeTest {
     public void tearDown(){
         driver.quit();
         driver = null;
+    }
+
+    @Test
+    public void should(){
+        driver.get("http://localhost:9999/");
+        List<WebElement> elementList = driver.findElements(By.className("input__control"));
+        elementList.get(0).sendKeys("Вася");
+        elementList.get(1).sendKeys("+79998776633");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+
+
     }
 }
